b6a2a07116a369839ed1e6132ddcecfdf7d4fda4
HeleneMG <helenemouttetg@gmail.com>
2019-11-05 16:06:58 +0100

    meta description accueil

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,7 +6,7 @@
          <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-         <meta name="description" content="description de la page" />
+         <meta name="description" content="Groupe de jazz sur la région de Marseille" />
          <title>Accueil - ZebraJazz</title>
          <script src="https://unpkg.com/scrollreveal"></script>
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
